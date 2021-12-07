c34d6114cff4d2ed36eca9c56e7033d4ae6d5fb9
DvdNss <private.david.naisse@gmail.com>
2021-12-07 21:28:08 +0100

    removed unecessary folder creation and added argument parser

---
+++ annots_to_csv.py
@@ -0,0 +1,92 @@
+import argparse
+import glob
+import os
+import xml.etree.ElementTree as ET
+
+import pandas as pd
+
+
+def xml_to_csv_and_ground_truth(path, output_path):
+    """
+    Returns a csv format from a given xml format dataset path.
+
+    """
+
+    # Annotations and labels lists
+    xml_list = []
+    labels = []
+
+    for xml_file in glob.glob(path + 'annotation/*.xml'):
+        # Parse the directory to find all annotations
+        tree = ET.parse(xml_file)
+
+        # Get file path
+        root = tree.getroot()
+
+        # Parse xml document to find the bbox and labels
+        for member in root.findall('object'):
+            bbx = member.find('bndbox')
+            xmin = int(float(bbx.find('xmin').text))
+            ymin = int(float(bbx.find('ymin').text))
+            xmax = int(float(bbx.find('xmax').text))
+            ymax = int(float(bbx.find('ymax').text))
+            label = member.find('name').text
+
+            # If label not in labels then add it
+            if label not in labels:
+                labels.append(label)
+
+            value = (os.path.abspath(path + "/image/" + root.find('filename').text),
+                     xmin,
+                     ymin,
+                     xmax,
+                     ymax,
+                     label
+                     )
+            # Append values to the xml list
+            xml_list.append(value)
+
+    # Create dataframe from generated lists
+    column_name = ['filename', 'xmin', 'ymin', 'xmax', 'ymax', 'label']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+
+    # Create label file
+    f = open(output_path + "labels.csv", "w+")
+
+    # Write labels
+    for i, label in enumerate(labels):
+        f.write(label + "," + str(i) + "\n")
+    f.close()
+
+    return xml_df
+
+
+def generate_csv_and_ground_truth(args=None):
+    """
+    Generates csv files and directories for all datasets mentioned in the config file.
+
+    """
+
+    # Create parser and its args
+    parser = argparse.ArgumentParser(description='Generate csv and ground truth files given datasets. ')
+    parser.add_argument('--train_dataset', help='Path to training dataset. ', default='dataset/train/')
+    parser.add_argument('--valid_dataset', help='Path to validation dataset. ', default='dataset/validation/')
+    parser.add_argument('--output_path', help='Output path for generated csv files. ', default='dataset/')
+
+    # Call parser args
+    parser = parser.parse_args(args)
+
+    for set in [parser.train_dataset, parser.valid_dataset]:
+        # Creates a dataframe from all xml files of a dataset
+        xmlDf = xml_to_csv_and_ground_truth(set, output_path=parser.output_path)
+
+        # Name csv file
+        csvName = parser.output_path + set.replace("dataset/", "").replace("/", "").replace("..", "") + ".csv"
+
+        # Convert dataframe to csv and save it
+        xmlDf.to_csv(f"{csvName}", index=False, header=False)
+        print(csvName, f"file has been added to {parser.output_path} directory. ")
+
+
+if __name__ == '__main__':
+    generate_csv_and_ground_truth()
