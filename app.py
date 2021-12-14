78ed252c47673d28e976bedace246d024e107017
DvdNss <private.david.naisse@gmail.com>
2021-12-15 00:52:16 +0100

    removed device_ids

--- app.py
+++ app.py
@@ -205,7 +205,6 @@ download_models(ids)
 # Model selection
 model_path = st.selectbox('Model selection', ('resnet50_20', 'resnet50_29', 'resnet152_20'), index=1)
 model = load_model(model_path=model_path)
-print(model.device_ids)
 
 if run:
     camera = cv2.VideoCapture(0)
