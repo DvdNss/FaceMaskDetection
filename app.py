c5ace13da6d8a92f6ccf95cae26b8a33cf855d33
DvdNss <private.david.naisse@gmail.com>
2021-12-15 00:57:39 +0100

    added discard model

--- app.py
+++ app.py
@@ -187,6 +187,16 @@ def process_img(model, image, labels, caption: bool = True):
     return image_orig
 
 
+def discard_model():
+    """
+    Remove model from memory.
+
+    :return:
+    """
+
+    del model
+
+
 # Page config
 st.set_page_config(layout="centered")
 st.title("Face Mask Detection")
@@ -203,7 +213,8 @@ ids = {
 download_models(ids)
 
 # Model selection
-model_path = st.selectbox('Model selection', ('resnet50_20', 'resnet50_29', 'resnet152_20'), index=1)
+model_path = st.selectbox('Model selection', ('resnet50_20', 'resnet50_29', 'resnet152_20'), index=1,
+                          on_change=discard_model())
 model = load_model(model_path=model_path)
 
 if run:
