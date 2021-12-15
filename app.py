37cdf1d4d2b62624d83cb9f8df032cd94f2734c6
DvdNss <private.david.naisse@gmail.com>
2021-12-15 13:54:39 +0100

    reverted changes

--- app.py
+++ app.py
@@ -10,13 +10,19 @@ import os.path
 import time
 
 import cv2
+import gdown
 import numpy as np
 import streamlit as st
 import torch
-import gdown
 
 
 def load_classes(csv_reader):
+    """
+    Load classes from csv.
+
+    :param csv_reader: csv
+    :return:
+    """
     result = {}
 
     for line, row in enumerate(csv_reader):
@@ -34,7 +40,17 @@ def load_classes(csv_reader):
     return result
 
 
+@st.cache
 def draw_caption(image, box, caption):
+    """
+    Draw caption and bbox on image.
+
+    :param image: image
+    :param box: bounding box
+    :param caption: caption
+    :return:
+    """
+
     b = np.array(box).astype(int)
     cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2)
     cv2.putText(image, caption, (b[0], b[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
@@ -58,31 +74,37 @@ def load_labels():
     return labels
 
 
+def download_models(ids):
+    """
+    Download all models.
+
+    :param ids: name and links of models
+    :return:
+    """
+
+    # Download model from drive if not stored locally
+    with st.spinner('Downloading models, this may take a minute...'):
+        for key in ids:
+            if not os.path.isfile(f"model/{key}.pt"):
+                url = f"https://drive.google.com/uc?id={ids[key]}"
+                gdown.download(url=url, output=f"model/{key}.pt")
+
+
 @st.cache(suppress_st_warning=True)
-def load_model(model_path, ids, prefix: str = 'model/'):
+def load_model(model_path, prefix: str = 'model/'):
     """
     Load model.
 
     :param model_path: path to inference model
-    :param ids: models ids
     :param prefix: model prefix if needed
     :return:
     """
 
-    url = f"https://drive.google.com/uc?id={ids[model_path]}"
-
-    # Download model from drive if not stored locally
-    if not os.path.isfile(f"{prefix}{model_path}.pt"):
-        with st.spinner('Downloading model, this may take a minute...'):
-            gdown.download(url=url, output=f"{prefix}{model_path}.pt")
-        st.success('Model loaded! ')
-
     # Load model
     if torch.cuda.is_available():
-        model = torch.load(f"{prefix}{model_path}.pt")
-        model.cuda()
+        model = torch.load(f"{prefix}{model_path}.pt").to('cuda')
     else:
-        model = torch.load(f"{prefix}{model_path}.pt", map_location=torch.device('cpu'))
+        model = torch.load(f"{prefix}{model_path}.pt", map_location=torch.device('cpu')).module.cpu()
     model.training = False
     model.eval()
 
@@ -105,19 +127,18 @@ def process_img(model, image, labels, caption: bool = True):
 
     smallest_side = min(rows, cols)
 
-    # rescale the image so the smallest side is min_side
+    # Rescale the image
     min_side = 608
     max_side = 1024
     scale = min_side / smallest_side
 
-    # check if the largest side is now greater than max_side, which can happen
-    # when images have a large aspect ratio
+    # Check if the largest side is now greater than max_side
     largest_side = max(rows, cols)
 
     if largest_side * scale > max_side:
         scale = max_side / largest_side
 
-    # resize the image with the computed scale
+    # Resize the image with the computed scale
     image = cv2.resize(image, (int(round(cols * scale)), int(round((rows * scale)))))
     rows, cols, cns = image.shape
 
@@ -168,35 +189,38 @@ def process_img(model, image, labels, caption: bool = True):
 
 # Page config
 st.set_page_config(layout="centered")
-st.title("Face Mask Detection")
-run = st.checkbox('Webcam mode')
-labels = load_labels()
+st.sidebar.title("Face Mask Detection")
 
 # Models drive ids
 ids = {
     'resnet50_20': '17c2kseAC3y62IwaRQW4m1Vc-7o3WjPdh',
-    'resnet50_29': '1E_IOIuE5OpO4tQgTbXjdAmXR-9BCxxmT',
+    # 'resnet50_29': '1E_IOIuE5OpO4tQgTbXjdAmXR-9BCxxmT',
     'resnet152_20': '1oUHqE_BgXehopdicuvPCGOxnwAdlDkEY',
 }
 
+# Download all models from drive
+download_models(ids)
+page = st.sidebar.selectbox('', options=('Description', 'Inference', 'Webcam'), index=0, help='Choose where to go. ')
+
 # Model selection
-model_path = st.selectbox('Model selection', ('resnet50_20', 'resnet50_29', 'resnet152_20'), index=1)
-model = load_model(model_path=model_path, ids=ids)
+labels = load_labels()
+model_path = st.sidebar.selectbox('Choose a model', ('resnet50_20', 'resnet152_20'), index=0)
+model = load_model(model_path=model_path) if model_path != '' else None
 
-if run:
-    camera = cv2.VideoCapture(-1)
+if page == 'Inference':
+    # Display example selection
+    index = st.number_input('', min_value=0, max_value=852, value=373, help='Choose an image. ')
 
-    # Process camera imgs
-    while run:
-        _, frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        st.image(process_img(model, frame, labels, caption=True))
-else:
-    index = st.number_input('', min_value=0, max_value=852, value=436)
+    # Get corresponding image and transform it
     image = cv2.imread(f'dataset/validation/image/maksssksksss{str(index)}.jpg')
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
     left, right = st.columns([3, 1])
+
+    # Draw img on left
     left.image(process_img(model, image, labels, caption=False))
+
+    # Write labels dict and device on right
     right.write({
         'green': 'with_mask',
         'orange': 'mask_weared_incorrect',
@@ -204,3 +228,27 @@ else:
     })
     device = 'CPU' if not torch.cuda.is_available() else 'GPU'
     right.write(f"CUDA: {torch.cuda.is_available()} ({device})")
+
+elif page == "Webcam":
+    try:
+        # Get webcam feed
+        camera = cv2.VideoCapture(0)
+
+        # Prepare video container
+        video = st.image([])
+
+        while page == "Webcam":
+            _, frame = camera.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            video.image(process_img(model, frame, labels, caption=True))
+    except:
+        st.warning(
+            'Unable to detect corresponding device. Note that this feature isn\'t available on Streamlit Cloud. ')
+
+elif page == 'Description':
+    st.title('Face Mask Detection')
+    st.image('resources/ex.jpg')
+    st.markdown(
+        "This project aims to create a Face Mask Detection model to visually detect facemasks on images and videos. "
+        "We operate with 3 labels: \n\n * _with_mask_ \n * _without_mask_\n * _mask_weared_incorrect_ \n\nThe dataset "
+        "contains approximately 2500 hand-collected and hand-labelled images.")
