d33d8b9401bea63e2916488c26f8fa12357315f4
DvdNss <private.david.naisse@gmail.com>
2021-12-15 04:18:31 +0100

    removed cache

--- app.py
+++ app.py
@@ -112,7 +112,6 @@ def load_model(model_path, prefix: str = 'model/'):
     return model
 
 
-@st.cache
 def process_img(model, image, labels, caption: bool = True):
     """
     Process img given a model.
@@ -202,8 +201,7 @@ ids = {
 
 # Download all models from drive
 download_models(ids)
-# page = st.sidebar.selectbox('', options=('Description', 'Inference', 'Webcam'), index=0, help='Choose where to go. ')
-page = st.sidebar.selectbox('', options=['Inference'], index=0, help='Choose where to go. ')
+page = st.sidebar.selectbox('', options=('Description', 'Inference', 'Webcam'), index=0, help='Choose where to go. ')
 
 # Model selection
 labels = load_labels()
@@ -232,26 +230,26 @@ if page == 'Inference':
     device = 'CPU' if not torch.cuda.is_available() else 'GPU'
     right.write(f"CUDA: {torch.cuda.is_available()} ({device})")
 
-# elif page == "Webcam":
-#     try:
-#         # Get webcam feed
-#         camera = cv2.VideoCapture(0)
-#
-#         # Prepare video container
-#         video = st.image([])
-#
-#         while page == "Webcam":
-#             _, frame = camera.read()
-#             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#             video.image(process_img(model, frame, labels, caption=True))
-#     except:
-#         st.warning(
-#             'Unable to detect corresponding device. Note that this feature isn\'t available on Streamlit Cloud. ')
-#
-# elif page == 'Description':
-#     st.title('Face Mask Detection')
-#     st.image('resources/ex.jpg')
-#     st.markdown(
-#         "This project aims to create a Face Mask Detection model to visually detect facemasks on images and videos. "
-#         "We operate with 3 labels: \n\n * _with_mask_ \n * _without_mask_\n * _mask_weared_incorrect_ \n\nThe dataset "
-#         "contains approximately 2500 hand-collected and hand-labelled images.")
+elif page == "Webcam":
+    try:
+        # Get webcam feed
+        camera = cv2.VideoCapture(0)
+
+        # Prepare video container
+        video = st.image([])
+
+        while page == "Webcam":
+            _, frame = camera.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            video.image(process_img(model, frame, labels, caption=True))
+    except:
+        st.warning(
+            'Unable to detect corresponding device. Note that this feature isn\'t available on Streamlit Cloud. ')
+
+elif page == 'Description':
+    st.title('Face Mask Detection')
+    st.image('resources/ex.jpg')
+    st.markdown(
+        "This project aims to create a Face Mask Detection model to visually detect facemasks on images and videos. "
+        "We operate with 3 labels: \n\n * _with_mask_ \n * _without_mask_\n * _mask_weared_incorrect_ \n\nThe dataset "
+        "contains approximately 2500 hand-collected and hand-labelled images.")
